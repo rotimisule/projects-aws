6cde1f3acd4015d889ed79cdface99da76841606
rotimisule <rotimisule24@gmail.com>
2023-08-10 01:02:24 +0000

    WEEK 20 project

--- week20project/variables.tf
+++ week20project/variables.tf
@@ -1,25 +1,25 @@
 #name of your ec2 instance
 variable "name" {
- type = string
- default = "rotimi-Jenkins"
+  type    = string
+  default = "rotimi-Jenkins"
 }
 # your availability zone 
 variable "az" {
- type = string
- default = "us-east-2a"
+  type    = string
+  default = "us-east-2a"
 }
 #your default vpc 
 variable "vpc_id" {
- type = string
- default = "<vpc-042c27a5f243b3ddd>"
+  type    = string
+  default = "<vpc-042c27a5f243b3ddd>"
 }
 #Instance type
 variable "type" {
- type = string
- default = "t2.micro"
+  type    = string
+  default = "t2.micro"
 }
 #s3 bucket, your globally unique bucket name
 variable "bucket" {
- type = string
- default = "<Rotimi-jenkins>"
+  type    = string
+  default = "<Rotimi-jenkins>"
 }
