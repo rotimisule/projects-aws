b33f6e80734f3aef864656e7189458af2b712510
rotimisule <rotimisule24@gmail.com>
2023-08-16 17:40:42 +0000

    week 20 jenkins script

--- WEEK 20 project/script.sh
+++ WEEK 20 project/script.sh
@@ -1,16 +1,26 @@
-#Jenkins bootstrap 
 #!/bin/bash
-#Updates the EC2 Instance
-sudo apt-get update -y
-#This is installing a java implementation
-sudo apt-get install openjdk-11-jre -y
-#Adds the jenkins repository
-sudo curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-sudo echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update -y
-sudo apt-get install fontconfig openjdk-11-jre -y
-#These commands install Jenkins
-sudo apt-get install jenkins -y
-sudo systemctl start jenkins
-sudo systemctl enable jenkins
 
+
+sudo apt-get update
+sudo apt-get install default-jdk -y
+
+
+sudo apt-get install maven -y
+
+# Script for Jenkins installation
+
+
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+
+
+
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+# Install Jenkins
+
+sudo apt-get update 
+sudo apt-get install jenkins -y
+sudo apt-get enable jenkins
